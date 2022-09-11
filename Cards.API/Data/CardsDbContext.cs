d2809d73fbb796a33f534dcdd4bbf26ff88300af
it18208436 <it18208436@my.sliit.lk>
2022-09-11 15:40:15 +0530

    Add project files.

---
+++ Cards.API/Data/CardsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cards.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.Data
+{
+    public class CardsDbContext : DbContext //CardsDbContext class inherit from DbContext class
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        // Dbset
+        public DbSet<Card> Cards { get; set; } //This Cards represent Sql server card table
+    }
+}
